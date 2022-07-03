d90868183a6b7c4ec116c736010d5fd8e09589ed
shueHeart <95758078+shueHeart@users.noreply.github.com>
2022-07-03 12:58:46 +0400

    Update ConcatMachine.java
    tabs

--- linkedlist/ConcatMachine.java
+++ linkedlist/ConcatMachine.java
@@ -3,24 +3,24 @@ package linkedlist;
 public class ConcatMachine {
 
 	public static LinkedList concat(LinkedList l1, LinkedList l2) {
-	
-	LinkedList concated = new LinkedList();		
-		
-    	if (l1.count() != l2.count()) return concated;
 
-    	Node nodeFromFirst = l1.head;
-    	Node nodeFromSecond = l2.head;
-    	    	
-    	while (nodeFromFirst != null && nodeFromSecond != null) {
-    		
-    		concated.addInTail(new Node(nodeFromFirst.value + nodeFromSecond.value));
-    		
-    		nodeFromFirst = nodeFromFirst.next;
-    		nodeFromSecond = nodeFromSecond.next;
-    		
+		LinkedList concated = new LinkedList();		
+
+		if (l1.count() != l2.count()) return concated;
+
+		Node nodeFromFirst = l1.head;
+		Node nodeFromSecond = l2.head;
+
+		while (nodeFromFirst != null && nodeFromSecond != null) {
+
+			concated.addInTail(new Node(nodeFromFirst.value + nodeFromSecond.value));
+
+			nodeFromFirst = nodeFromFirst.next;
+			nodeFromSecond = nodeFromSecond.next;
+
+		}
+
+		return concated;
+    	
     	}
-    	
-    	return concated;
-    	
-    }
 }
