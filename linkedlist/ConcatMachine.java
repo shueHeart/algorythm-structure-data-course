b574c2f54b43b414e44ddf6cce1860b7305125ab
shueHeart <95758078+shueHeart@users.noreply.github.com>
2022-07-03 12:56:14 +0400

    Update ConcatMachine.java
    fix

--- linkedlist/ConcatMachine.java
+++ linkedlist/ConcatMachine.java
@@ -3,11 +3,11 @@ package linkedlist;
 public class ConcatMachine {
 
 	public static LinkedList concat(LinkedList l1, LinkedList l2) {
+	
+	LinkedList concated = new LinkedList();		
 		
     	if (l1.count() != l2.count()) return new LinkedList();
 
-    	LinkedList concated = new LinkedList();
-    	
     	Node nodeFromFirst = l1.head;
     	Node nodeFromSecond = l2.head;
     	    	
