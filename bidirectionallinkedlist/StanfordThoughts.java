d1d42c0cfbb4e34c2eb79b310cdef2a4f636f5dc
shueHeart <95758078+shueHeart@users.noreply.github.com>
2022-07-08 19:52:20 +0400

    Update StanfordThoughts.java
    update

--- bidirectionallinkedlist/StanfordThoughts.java
+++ bidirectionallinkedlist/StanfordThoughts.java
@@ -1,53 +1,58 @@
-package bidirectionallinkedlist;
-
 import java.util.ArrayList;
 
 
 public class StanfordThoughts {
-	  	public Node head;
-	    public Node tail;
+	  	private Node head;
+	    private Node tail;
 
-	     public StanfordThoughts()
-	     {
-	       head = new Node();
-	       tail = new Node();
-	     }
-
-	     public void addInTail(Node _item)
-	     {
-	    	 _item.next = this.tail;
-	    	 this.tail.prev.next = _item;
-	    	 _item.prev = this.tail.prev;
-	     }
-
-	     public Node find(int _value)
-	     {
-	    	 Node node = this.head.next;
-	    	 
-	    	 while (!node.dummy) { 
-	    		 if (node.value == _value) return node;
-	    	 }
-	    	 
-	    	 return null;
-	     }
-
-	     public ArrayList<Node> findAll(int _value)
-	     {
+		 public StanfordThoughts()
+		 {
+		   head = new Node();
+		   tail = new Node();
+		   
+		   this.head.next = this.tail;
+		   this.tail.prev = this.head;
+		 }
+		
+		 public void addInTail(Node _item)
+		 {
+			 
+			 _item.next = this.tail;
+			 this.tail.prev.next = _item;
+			 _item.prev = this.tail.prev;
+			 this.tail.prev = _item;
+		 }
+		
+		 public Node find(int _value)
+		 {
+			 Node node = this.head.next;
+			 
+			 while (node != null) { 
+				 if (node.value == _value) return node;
+				 node = node.next;
+			 }
+			 
+			 return null;
+		 }
+		
+		 public ArrayList<Node> findAll(int _value)
+		 {
 			ArrayList<Node> nodes = new ArrayList<Node>();
 			   
 			Node node = this.head.next;
 			   
-			while (!node.dummy) {
+			while (node != null) {
 				if (node.value == _value) nodes.add(node);
+				node = node.next;
 			}
 			return nodes;
-	     }
+		 }
 
 		public boolean remove(int _value)
 		{
 			Node node = this.head.next;
 			
-			while (!node.dummy) {
+			while (node != null) {
 				
 				if (node.value != _value) {
 					node = node.next;
@@ -55,8 +60,7 @@ public class StanfordThoughts {
 				}
 				
 				node.next.prev = node.prev;
-				node.prev = node.next;
-				
+				node.prev.next = node.next;
 				
 				return true;
 				
@@ -78,7 +82,9 @@ public class StanfordThoughts {
 	    		 }
 	    		 
 	    		 node.next.prev = node.prev;
-	    		 node.prev = node.next;
+	    		 node.prev.next = node.next;
+	    		 
+	    		 node = node.next;
 	    	 }
 	    	 
 	    	 
@@ -106,22 +112,36 @@ public class StanfordThoughts {
 
 		public void insertAfter(Node _nodeAfter, Node _nodeToInsert)
 		{
+			if (_nodeAfter == null) {
+				this.head.next.prev = _nodeToInsert;
+				_nodeToInsert.next = this.head.next;
+				this.head.next = _nodeToInsert;
+				_nodeToInsert.prev = this.head;
+				return;
+			}
 			
 			_nodeToInsert.prev = _nodeAfter;			
 			_nodeToInsert.next  = _nodeAfter.next;
 			_nodeToInsert.next.prev = _nodeToInsert;
 			_nodeAfter.next = _nodeToInsert;
-
-			
 		}
+		
+		public Node getHead() {
+			return this.head.next;
+		}
+		
+		public Node getTail() {
+			return this.tail.prev;
+		}
+		
 }
 
-class Node
+public class Node
 {
-    public boolean dummy;
-    public int value;
-    public Node next;
-    public Node prev;
+    protected boolean dummy;
+    protected int value;
+    protected Node next;
+    protected Node prev;
     
     public Node () {
     	dummy = true;
@@ -130,6 +150,28 @@ class Node
     public Node (int value) {
     	this.value = value;
     	dummy = false;
+    }
+    
+    public Node getNext () {
+    	if (!next.dummy) return next;
+    	return null;
+    }
+    
+    public void setNext (Node next) {
+    	this.next = next;
+    }
+    
+    public Node getPrev () {
+    	if (!prev.dummy) return prev;
+    	return null;
+    }
+    
+    public void setPrev (Node prev) {
+    	this.prev = prev;
+    }
+    
+    public int getValue () {
+    	return value;
     }
   
 }
