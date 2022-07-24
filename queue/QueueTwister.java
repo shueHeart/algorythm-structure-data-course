a5bf3805dc53499cc1c766feff5a925d3dc63b81
shueHeart <95758078+shueHeart@users.noreply.github.com>
2022-07-25 00:06:03 +0400

    Add files via upload

---
+++ queue/QueueTwister.java
@@ -0,0 +1,16 @@
+package queue;
+
+public class QueueTwister {
+	public static Queue twist (Queue queue, int N) {
+		
+		if (queue.size() == 0) return null;
+		
+		int realTwist = N % queue.size();
+		
+		
+		
+		Queue twistedQueue = new Queue<queue.>;
+		
+		
+	}
+}
