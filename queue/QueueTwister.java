4de7f04c5f1bebecfd1e2641b50886f79deff92d
shueHeart <95758078+shueHeart@users.noreply.github.com>
2022-07-26 00:48:31 +0400

    Update QueueTwister.java
    ready

--- queue/QueueTwister.java
+++ queue/QueueTwister.java
@@ -1,16 +1,34 @@
 package queue;
 
 public class QueueTwister {
-	public static Queue twist (Queue queue, int N) {
+	public static void twist (Queue queue, int N) {
 		
-		if (queue.size() == 0) return null;
+		if (queue.size() == 0) return;
 		
 		int realTwist = N % queue.size();
+				
+		Queue tail = new Queue();
+		Queue head = new Queue();
 		
+		for (int i = 0; i < realTwist; ++i) {
+			tail.enqueue(queue.dequeue());
+		}
 		
+		int size = queue.size();
+		for (int i = 0; i < size; ++i) {
+			head.enqueue(queue.dequeue());
+		}
 		
-		Queue twistedQueue = new Queue<queue.>;
+		size = head.size();
+		for (int i = 0; i < size; ++i) {
+			queue.enqueue(head.dequeue());
+		}
 		
+		size = tail.size();
+		for (int i = 0; i < size; ++i) {
+			
+			queue.enqueue(tail.dequeue());
+		}
 		
 	}
 }
