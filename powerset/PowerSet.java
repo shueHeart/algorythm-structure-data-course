abd7f4ccdee9ed1a39ceddd627fac1cebb5fb637
shueHeart <95758078+shueHeart@users.noreply.github.com>
2022-08-17 19:35:54 +0400

    Update PowerSet.java
    fix

--- powerset/PowerSet.java
+++ powerset/PowerSet.java
@@ -1,7 +1,3 @@
-package powerset;
-
-import java.util.*;
-
 public class PowerSet {
 
 	public List<String> storage;
@@ -11,31 +7,22 @@ public class PowerSet {
 	}
 
 	public int size() {
-		// количество элементов в множестве
 		return storage.size();
 	}
 	
 	public void put(String value) {
-		
 		if (!storage.contains(value)) storage.add(value);
-		// всегда срабатывает
 	}
 
 	public boolean get(String value) {
-		// возвращает true если value имеется в множестве,
-		// иначе false
 		return storage.contains(value);
 	}
 
-	public boolean remove(String value) {
-		// возвращает true если value удалено
-		// иначе false
-		
+	public boolean remove(String value) {		
 		return storage.remove(value);
 	}
 
 	public PowerSet intersection(PowerSet set2) {
-		// пересечение текущего множества и set2
 		PowerSet inter = new PowerSet();
 		for (String string : storage) { 
 			if (set2.get(string)) inter.put(string); 
@@ -45,7 +32,6 @@ public class PowerSet {
 	}
 
 	public PowerSet union(PowerSet set2) {
-		// объединение текущего множества и set2
 		
 		PowerSet ps = new PowerSet();
 		
@@ -56,7 +42,6 @@ public class PowerSet {
 	}
 
 	public PowerSet difference(PowerSet set2) {
-		// разница текущего множества и set2
 		
 		PowerSet diff = new PowerSet();
 		for (String string : storage) { 
@@ -67,17 +52,12 @@ public class PowerSet {
 	}
 
 	public boolean isSubset(PowerSet set2) {
-		// возвращает true, если set2 есть
-		// подмножество текущего множества,
-		// иначе false
 		
 		if (size() < set2.size()) return false;
 		
 		for (String str : set2.storage) {
 			if (!get(str)) return false;
 		}
-		
-		
 		
 		return true;
 	}
